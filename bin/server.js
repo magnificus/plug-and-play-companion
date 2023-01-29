7707c7253f8c7eb8210db772eaca3eee05490f20
Tobias Elinder <tobiaselinder@gmail.com>
2023-01-29 17:37:59 +0100

    build hell

--- bin/server.js
+++ bin/server.js
@@ -1,56 +1,42 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-const express_1 = __importDefault(require("express"));
-const xml_js_1 = require("xml-js");
-var cors = require('cors');
-var app = (0, express_1.default)();
+import express from "express";
+import { xml2json } from 'xml-js';
+import fetch from 'node-fetch';
+import cors from 'cors';
+var app = express();
 const port = 6655;
-function convertResponseToJSON(response) {
+function convertResponseToJSON(responseText) {
     try {
-        if (typeof response == "object") {
-            return response;
-        }
-        else if (typeof response == "string") {
-            return (0, xml_js_1.xml2json)(response);
-        }
+        return JSON.parse(responseText);
     }
     catch (e) {
-        console.error("Couldn't parse response");
-        return response;
+        try {
+            return xml2json(responseText);
+        }
+        catch (e) {
+            console.error("Couldn't parse response");
+        }
     }
+    return responseText;
 }
 // allow requests from anywhere (even though itll most likely be localhost)
 app.use(cors({
     origin: '*'
 }));
-app.use(express_1.default.json());
-app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.use(express.json());
+app.get("/", async (req, res) => {
     const forwardedHeaders = JSON.parse(req.get("forwardedHeaders"));
     const finalURL = forwardedHeaders.finalURL;
     delete forwardedHeaders.finalURL;
     const otherHeaders = Object.keys(forwardedHeaders);
     //console.log("other headers: " + otherHeaders);
     //console.log("final URL: " + finalURL)
-    const finalRes = yield axios_1.default.get(finalURL);
-    const converted = convertResponseToJSON(finalRes.data);
+    const finalRes = await fetch(finalURL);
+    const converted = convertResponseToJSON(await finalRes.text());
     // parse XML if neccessary
-    console.log(finalRes.data);
+    //console.log(finalRes.data);
     res.headers = otherHeaders;
     return res.json(converted);
-}));
+});
 app.post("/", (req, res) => {
     console.log("ya tryin to post??");
     res.send({ "Response": "PostMate" });
