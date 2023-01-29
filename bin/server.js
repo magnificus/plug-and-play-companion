cff85b9d7674e4d44f4e3d7eb56c24be171baf04
Tobias Elinder <tobiaselinder@gmail.com>
2023-01-29 14:13:12 +0100

    nodemon

--- bin/server.js
+++ bin/server.js
@@ -1,2 +1,16 @@
 "use strict";
+var express = require('express');
 console.log("ayy caramba");
+var cors = require('cors');
+var app = express();
+const port = 6655;
+// allow requests from anywhere (even though itll most likely be localhost)
+app.use(cors({
+    origin: '*'
+}));
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+app.listen(port, () => {
+    console.log("running server");
+});
